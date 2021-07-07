e14ab9dbe18475ac65a7c452d2211cda0b86bfb3
Rodrigo Gutierrez <rodrigo.gutierrez@up.ac.pa>
2021-07-07 07:17:25 -0500

    🔨 Refactor Code:  Actividad 8 - Logica de indices al cambiar imagen y palabras - Imagen inicial y cilabas por fecto

--- js/actividad8.js
+++ js/actividad8.js
@@ -1,20 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('.container');
   const imagen = document.getElementById('imagen');
-  let cont = 2; //Indice de imagen
-  let cantImagen = 7; //Cantidad total de imagen
-
-  imagen.src = `../img/act8/img1.jpg`;
+  const silaba = document.querySelector('.silaba');
+  let cont = 1; //Indice de imagen
+  let cantImagen = 6; //Cantidad total de imagen a mostrar
+  let fraces = [
+    ['Pe', 'rro'],
+    ['Del', 'fin'],
+    ['nos', 'u'],
+    ['a', 'ho', 'ra'],
+    ['ne', 'car'],
+    ['ce', 'rra', 'du', 'ras'],
+  ];
 
   container.addEventListener('click', (e) => {
     //Accion del boton next
     if (e.target.classList.contains('btn-next')) {
+      //   Ocultamos el boton
+      if (cont === cantImagen) {
+        hiddenBtnNext(e);
+      }
       if (cont <= cantImagen) {
+        //Eliminar elementos del div/silabas
+        silaba.innerHTML = '';
+
+        //Cambio de imagen
         cambioImagen(cont);
+
+        //Agregrar las cilabas
+        fraces[cont - 1].forEach((element) => {
+          let parrafo = document.createElement('p');
+          parrafo.textContent = element;
+          silaba.appendChild(parrafo);
+          //   console.log(element);
+        });
         cont++;
-      } else {
-        hiddenBtnNext();
-        cont = 1;
       }
     }
   });
@@ -25,6 +45,14 @@ function cambioImagen(cont) {
   imagen.src = `../img/act8/img${cont}.jpg`;
 }
 //Ocultar boton next
-function hiddenBtnNext() {
+function hiddenBtnNext(e) {
   e.target.style.visibility = 'hidden';
+}
+
+function aliminarHijos() {
+  var padre = document.querySelector('.silaba');
+  for (var i = 0; i < padre.children.length; i++) {
+    var hijo = padre.children[i];
+    padre.remove(hijo);
+  }
 }
