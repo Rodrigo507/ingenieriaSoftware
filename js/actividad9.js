578e68d2f1b3cfc9241f6a65b8925ef336146ddd
Rodrigo Gutierrez <rodrigo.gutierrez@up.ac.pa>
2021-07-13 08:11:04 -0500

    ✨ New Feature: Actividad 9 - Cambia la cantidad de enunciado faltantes

--- js/actividad9.js
+++ js/actividad9.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('.container');
   const audio = document.getElementById('audio');
-
+  const cantActividad = document.querySelector('.cantPregunta');
   let cont = 2; //Indice de audio
   let cantAudio = 2; //Cantidad total de audio a rproducir dinamicamente
 
+  cambioCantidadTexto(1);
   container.addEventListener('click', (e) => {
     //Accion del boton next
     if (e.target.classList.contains('btn-next')) {
       //   Ocultamos el boton
       if (cont <= cantAudio) {
         cambioAudio(cont);
+        cambioCantidadTexto(cont);
         if (cont === cantAudio) {
           hiddenBtnNext(e);
         }
@@ -18,6 +20,10 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   });
+  //Cambia la cantidad de enunciado faltantes
+  function cambioCantidadTexto(cant) {
+    cantActividad.innerHTML = `${cant}/${cantAudio}`;
+  }
 });
 
 //Ocultar boton next
