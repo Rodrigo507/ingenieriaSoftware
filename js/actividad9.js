8a2234e961ddb1aff73de5ca818d3f7ee9916527
Rodrigo Gutierrez <rodrigo.gutierrez@up.ac.pa>
2021-07-14 01:58:02 -0500

    ✨ New Feature: Actividad 9 - Modals - Evaluacion

--- js/actividad9.js
+++ js/actividad9.js
@@ -2,37 +2,85 @@ document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('.container');
   const audio = document.getElementById('audio');
   const cantActividad = document.querySelector('.cantPregunta');
+  const btnterm = document.querySelector('.btn-term');
+  const modalResult = document.getElementById('modal-body');
   let cont = 2; //Indice de audio
   let cantAudio = 2; //Cantidad total de audio a rproducir dinamicamente
+  var conrrectasCont = 0;
+  var indiceGeneral = 0;
+  const correctas = [
+    'Me contaron que hace tiempo durante una calida noche de verano una estrella recien nacida ilumino de tal manera a una luciernaga que volaba en la tranquilidad de la campiña',
+    'Que parecía haberla envuelto en una autentico abrazo de luz con voz suave pero profunda',
+  ];
 
   cambioCantidadTexto(1);
+  // $('#myModal').modal('show');
   container.addEventListener('click', (e) => {
     //Accion del boton next
     if (e.target.classList.contains('btn-next')) {
-      //   Ocultamos el boton
-      if (cont <= cantAudio) {
+      evaluacion(indiceGeneral);
+      if (indiceGeneral === cantAudio - 2) {
+        hiddenBtnNext(e);
+        btnterm.removeAttribute('hidden');
+      }
+      if (indiceGeneral <= cantAudio) {
         cambioAudio(cont);
         cambioCantidadTexto(cont);
-        if (cont === cantAudio) {
-          hiddenBtnNext(e);
-        }
-        cont++;
       }
+      // if (cantAudio === x) {
+      //   evaluacion(cont);
+      //   hiddenBtnNext(e);
+      //   btnterm.removeAttribute('hidden');
+      // }
+
+      // //   Ocultamos el boton
+      // if (cont <= cantAudio) {
+      //   evaluacion(cont);
+      //   cambioAudio(cont);
+      //   cambioCantidadTexto(cont);
+      // }
+      indiceGeneral++;
+      cont++;
+    }
+    if (e.target.classList.contains('btn-term')) {
+      let contenido = '';
+      evaluacion(indiceGeneral);
+      hiddenBtnNext(e);
+      contenido += `Total de puntos obtenidos: ${conrrectasCont} <br><br> <p>Respuestas correctas</p>`;
+      correctas.forEach((element) => {
+        contenido += `<li>${element}</li> ` + '<br>';
+      });
+
+      modalResult.innerHTML = contenido;
     }
   });
   //Cambia la cantidad de enunciado faltantes
   function cambioCantidadTexto(cant) {
     cantActividad.innerHTML = `${cant}/${cantAudio}`;
   }
+  //Ocultar boton next
+  function hiddenBtnNext(e) {
+    e.target.style.visibility = 'hidden';
+  }
+
+  //Cambio de audio
+  function cambioAudio(cont) {
+    audio.src = `../audio/part${cont}.mp3`;
+  }
+
+  function evaluacion(indice) {
+    var inputs = document.querySelector('textarea');
+    var audioCorrecto = new Audio('../audio/correcto.mp3');
+    var audioIncorrecto = new Audio('../audio/incorrecto.mp3');
+    // console.log(inputs.value.toLowerCase());
+    // console.log(correctas[0].toLowerCase());
+
+    if (correctas[indice].toLowerCase() === inputs.value.toLowerCase()) {
+      audioCorrecto.play();
+      conrrectasCont++;
+    } else {
+      audioIncorrecto.play();
+    }
+    inputs.value = '';
+  }
 });
-
-//Ocultar boton next
-function hiddenBtnNext(e) {
-  e.target.style.visibility = 'hidden';
-}
-
-//Cambio de audio
-function cambioAudio(cont) {
-  audio.src = `../audio/part${cont}.mp3`;
-  console.log(audio.src);
-}
